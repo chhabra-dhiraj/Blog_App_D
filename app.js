b2b71e949d56f50b607f93a0468a08975ac00361
Dhiraj Chhabra <dhirajchhabra630@gmail.com>
2019-01-06 05:22:55 +0530

    Starter UI for all the screens is added with dropdown menu not working and small ui faults

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var express = require('express'),
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 app.use(methodOverride("_method"));
 
 // *Database setting*
---
+++ public/scripts/show.js
@@ -0,0 +1,3 @@
+$('.ui.dropdown')
+    .dropdown();
+console.log("dhiraj");
